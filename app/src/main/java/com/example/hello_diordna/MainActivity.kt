2d47a4a7a959f2f42b29b5e798b4c3951187e5b1
1mean <yiming.dong.2018@gmail.com>
2021-11-13 15:37:47 +0800

    create new project names helo-diordna

---
+++ app/src/main/java/com/example/hello_diordna/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.hello_diordna
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onRestart() {
+
+        super.onRestart()
+
+    }
+
+}
